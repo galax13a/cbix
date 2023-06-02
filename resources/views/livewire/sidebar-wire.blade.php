91f37fbfcd76808065bcd0d69148c6bdec2735af
Cristhian <galax13a@yahoo.es>
2023-06-01 20:45:17 -0500

    v1.3.11 - menu wire sidebar

--- resources/views/livewire/sidebar-wire.blade.php
+++ resources/views/livewire/sidebar-wire.blade.php
@@ -3,10 +3,10 @@
         <!-- Sidebar -->
         <div :class="{ 'd-none': !open_sidebar_wire, '': open_sidebar_wire }" class="border-right sidebar-sticky floating-menu" id="sidebar-wrapper">
             <div class="list-group list-group-flush rounded-2">
-                <a href="{{ url('/home') }}"  class="list-group-item list-group-item-action custom-link"> 🎯 Dashboard</a>
-                <a href="#" class="list-group-item list-group-item-action custom-link ">😺 My Bio*s</a>                
-                <a href="/api-chaturbate/" class="list-group-item list-group-item-action custom-link ">🎨 Api Chaturbate</a>                
-                <a href="/account/" class="list-group-item list-group-item-action custom-link"> 🏅 ⛔️Account <br><span class="bg-danger p-1 text-white fw-bold rounded-3">No Completed</span> </a>   
+                <a href="{{ url('/home') }}"  class="list-group-item list-group-item-action"> 🎯 Dashboard</a>
+                <a href="#" class="list-group-item list-group-item-action ">😺 My Bio*s</a>                
+                <a href="/api-chaturbate/" class="list-group-item list-group-item-action">🎨 Api Chaturbate</a>                
+                <a href="/account/" class="list-group-item list-group-item-action > 🏅 ⛔️Account <br><span class="bg-danger p-1 text-white fw-bold rounded-3">No Completed</span> </a>   
                 <a href="javascript:void(0)" class="list-group-item"><livewire:com-dark-wire /></a> 
                 <a href="javascript:void(0)" class="navbar-brand mb-0 h3  p-2 shadow-lg list-group-item "> </a> 
                               
