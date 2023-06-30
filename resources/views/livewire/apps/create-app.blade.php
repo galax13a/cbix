aa1f047b1b47b3925ba758823a948df6b4d8e7d2
Cristhian <galax13a@yahoo.es>
2023-06-30 11:21:42 -0500

    v1.9.8 apps  interfase btn new

--- resources/views/livewire/apps/create-app.blade.php
+++ resources/views/livewire/apps/create-app.blade.php
@@ -1,14 +1,14 @@
 <div class="row align-content-center container-sm">
-   
-    <div class="col-md-12 shadow rounded-4 p-4 ">
+
+    <div class="col-12 shadow rounded-4 p-4 ">
         <form wire:submit.prevent="create1">
             <div class="row g-3 align-items-center">
                 <div class="input-group input-group-lg">
-                    <span class="input-group-text text-bold text-warning" id="inputGroup-sizing-lg">
+                    <span class="bg-black input-group-text text-bold text-warning" id="inputGroup-sizing-lg">
                         🍓 Name App
                     </span>
-                    <input type="text" class="form-control shadow" wire:model="name"
-                        aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg">
+                    <input type="text" class="form-control shadow" wire:model="name" aria-label="Sizing example input"
+                        aria-describedby="inputGroup-sizing-lg">
                 </div>
                 <div class="col-auto">
                     <span id="passwordHelpInline" class="form-text">
@@ -16,31 +16,36 @@
                     </span>
                 </div>
             </div>
-            <div class="row g-3 align-items-center mt-3">
-                <div class="input-group input-group-lg">
-                    <span class="input-group-text text-bold text-warning" id="inputGroup-sizing-lg">
-                        🌐 Slug
-                    </span>
-                    <input type="text" class="form-control shadow" wire:model="slug"
-                        aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg">
-                </div>
-                <div class="col-auto">
-                    <span id="slugHelpInline" class="form-text">
-                        Auto-generated from Name App.
-                    </span>
-                </div>
+            <div class="row g-3 align-items-center mt-1">
+
+                <span class="input-group-text text-bold bg-light" id="inputGroup-sizing-lg">
+                    <strong class="p-1 rounded-4" >
+                        🌐 Slug: {{ url('/') }}/apps/{{ $slug }}
+                    </strong>
+                </span>
+
+
             </div>
-            @if ($slug && $this->slugExists($slug))
-                <p class="text-danger">Slug already exists. Please choose a different one.</p>
-            @endif
-            <button id="btn-new" type="submit" class="p-2 m-2 rounded-3 shadow-sm"
-            @if ($slug && $this->slugExists($slug)) disabled @endif>
-            <strong>Create 🍒</strong>
-    </button>
-        </form>
+            <div class="col-auto">
+                <span id="slugHelpInline" class="form-text">
+                    Auto-generated from Name App.  {{ $this->name }}
+
+                </span>
+                @if ($slug && $this->slugExists($slug))
+                    <p class="text-danger shadow-sm p-2 rounded-3">Slug app, already exists. Please choose a different
+                        one.</p>
+                @endif
+            </div>
     </div>
-    
-    
+
+    <button id="btn-new" type="submit" class="p-2 m-2 rounded-3 shadow-sm"
+        @if (!$slug || $this->slugExists($slug)) disabled @endif>
+        <strong>Create App 🍒</strong>
+    </button>
+
+    </form>
+
+
 
 </div>
 </div>
