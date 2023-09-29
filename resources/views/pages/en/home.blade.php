8efd6014e15ec5808ac904952c990c52e4858de1
Cristhian <galax13a@yahoo.es>
2023-09-29 08:16:18 -0500

    v1.8.7.1 folio home landing complile home

--- resources/views/pages/en/home.blade.php
+++ resources/views/pages/en/home.blade.php
@@ -1,8 +1,9 @@
     @extends('layouts.tema.app')
     @section('title', __('Home Donwload AppStudio'))
+     {{--
     @push('scripts-head')
         {{-- <link href="{{ asset('cs/home.css') }}" rel="stylesheet"> --}}
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+      {{--   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
             integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     @endpush
     @push('scripts-body')
@@ -10,7 +11,7 @@
             integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
         </script>
     @endpush
-
+--}}
     @section('content')
         <x-themacoms.navbar-flex />           
         <<x-themacoms.btnup/>
