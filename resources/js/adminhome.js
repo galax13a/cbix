fb061a9d221dddfb1bc144615163d1eee4d6d276
Cristhian <galax13a@yahoo.es>
2023-10-01 16:34:56 -0500

    v1.9.0.5 bugs app livewire dashboard

--- resources/js/adminhome.js
+++ resources/js/adminhome.js
@@ -1,3 +1,8 @@
+import * as bootstrap from 'bootstrap'
+window.bootstrap = bootstrap;
+import Notiflix from 'notiflix';
+window.Notiflix = Notiflix;
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'boxicons/css/boxicons.min.css';
 
@@ -5,8 +10,3 @@ import 'bootstrap/dist/js/bootstrap.min.js';
 import 'boxicons/dist/boxicons.js';
 
 import './lib/admin-script.js';
-import * as bootstrap from 'bootstrap'
-window.bootstrap = bootstrap;
-
-import Notiflix from 'notiflix';
-window.Notiflix = Notiflix;
