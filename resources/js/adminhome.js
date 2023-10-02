efa8f1a7a766084731caa9be70c3532c79825c1c
Cristhian <galax13a@yahoo.es>
2023-10-01 22:00:23 -0500

    v1.9.0.7 bugs appbug menu bootrap bugs dark

--- resources/js/adminhome.js
+++ resources/js/adminhome.js
@@ -1,4 +1,4 @@
-//import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import 'boxicons/css/boxicons.min.css';
 import * as bootstrap from 'bootstrap'
 window.bootstrap = bootstrap;
